5199706fb9ea7c94dadae96e5da2fe9ffa548839
Arul Amudhan G <garulamudhan@gmail.com>
2026-07-07 15:53:20 +0530

    chore: Clean up legacy stray files from project root

--- add_rejected_area.sql
+++
@@ -1,3 +0,0 @@
-USE pharma_ims;
-INSERT IGNORE INTO Location_Master (location_code, location_name, description, capacity) 
-VALUES ('REJECTED_AREA', 'Rejected Area', 'Isolation area for non-conforming materials', 1000);
